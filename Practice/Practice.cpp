c6cc3ce399a8a9a613d7ece00bb12b4584c73b44
ohne <ghdlr785@gmail.com>
2022-03-18 15:31:15 +0900

    선택정렬

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -15,6 +15,8 @@ void Swap(int& value1, int& value2)
     value2 = temp;
 }
 // Sequential sort
+// 시간복잡도 : O(n^2)
+// 공간복잡도 : O(n)
 void SequentialSort(int input[], int size)
 {
     for (int i = 0; i < size-1; i++)
@@ -25,6 +27,28 @@ void SequentialSort(int input[], int size)
             {
                 Swap(input[i], input[j]);
             }
+        }
+    }
+}
+
+// Selection Sort
+// 시간복잡도  : O(n^2)
+// 공간복잡도  : O(n)
+void SelectionSort(int input[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        int minIndex = 1;
+        for (int j = i; j < size; j++)
+        {
+            if (input[minIndex] > input[j])
+            {
+                minIndex = j;
+            }
+        }
+        if (minIndex != i)
+        {
+            Swap(input[i], input[minIndex]);
         }
     }
 }
