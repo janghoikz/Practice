c7196450505a401e23c36cb04b60bab70818a981
ohne <ghdlr785@gmail.com>
2022-03-18 15:15:59 +0900

    순차정렬

--- Practice/Practice.cpp
+++ Practice/Practice.cpp
@@ -1,7 +1,40 @@
 ﻿#include <iostream>
 
-int main()
+void PrintArray(int input[], int size)
 {
-    std::cout << "Hello World!\n";
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << input[i] << " ";
+    }
+    std::cout << std::endl;
+}
+void Swap(int& value1, int& value2)
+{
+    int temp = value1;
+    value1 = value2;
+    value2 = temp;
+}
+// Sequential sort
+void SequentialSort(int input[], int size)
+{
+    for (int i = 0; i < size-1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (input[i] > input[j])
+            {
+                Swap(input[i], input[j]);
+            }
+        }
+    }
 }
 
+int main()
+{
+    const int SIZE{ 5 };
+    int array[SIZE]{ 8, 7, 2, 3, 1 };
+
+    //Sort
+
+    PrintArray(array, SIZE);
+}
